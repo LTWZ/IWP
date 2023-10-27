4f11ee4c8b360ff644f83cd8f9c9cbfbab316a52
LTWZ <leroytan05@gmail.com>
2023-10-27 22:46:00 +0800

    misc stuff

--- Assets/Scripts/Entity/Enemy_1.cs
+++ Assets/Scripts/Entity/Enemy_1.cs
@@ -1,34 +1,97 @@
-
+using Pathfinding;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class Enemy_1 : EnemyEntity
 {
-    public float patrolSpeed = 2.0f;
-    public float chaseSpeed = 4.0f;
-    public float detectionDistance = 5.0f;
-    public float returnToPatrolDistance = 8.0f;
-    public Transform[] patrolPoints; // Assign patrol points in the Inspector.
 
-    private Transform player;
-    private Rigidbody2D rb;
-    private int currentPatrolPoint = 0;
-    private bool isChasing = false;
+    public Transform target;
+
+    public float enemyspeed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    public Transform enemyGFX;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath;
+
+    Seeker seeker;
+    Rigidbody2D rb;
 
     [Header("HP Code")]
     private TextMeshProUGUI HB_valuetext;
 
     private void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        rb = GetComponent<Rigidbody2D>();
         currHealth = Hp;
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
+
+        if (rb.velocity.x >= 0.01f)
+        {
+            enemyGFX.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (rb.velocity.x <= -0.01f)
+        {
+            enemyGFX.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
     }
 
 
